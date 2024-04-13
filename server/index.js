1cc44e6692b17384981c5b701032bd95ac009ded
Tinsa <tinsa.baby@whitehatjr.com>
2024-04-13 18:01:49 +0530

    sample socketio demo based on https://dev.to/novu/building-a-chat-app-with-socketio-and-react-2edj

---
+++ server/index.js
@@ -0,0 +1,49 @@
+//https://dev.to/novu/building-a-chat-app-with-socketio-and-react-2edj
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+let users = [];
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('message', (data) => {
+        socketIO.emit('messageResponse', data);
+        console.log(data);
+      });
+       //Listens when a new user joins the server
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+})
